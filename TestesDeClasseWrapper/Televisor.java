0aadfe72c9fe5f64a680d294cca38b55d3dd7d37
Micael Marques <micaelf.yo18@gmail.com>
2024-06-16 17:49:43 -0300

    atualização

--- TestesDeClasseWrapper/Televisor.java
+++ TestesDeClasseWrapper/Televisor.java
@@ -1,2 +1,41 @@
-package TestesDeClasseWrapper;public class Televisor {
+package TestesDeClasseWrapper;
+
+class Televisor {
+
+    Integer canal = 130;
+    Integer volume = 20;
+
+    void mudarCanal(Integer novoCanal) {
+        if (canal.equals(novoCanal)) {
+            System.out.println("Novo canal é também o canal atual.");
+        } else {
+            canal = novoCanal;
+            System.out.println("Canal alterado para " + canal);
+        }
+    }
+
+    void mudarVolume(Integer novoVolume) {
+        if (volume.equals(novoVolume)) {
+            System.out.println("Novo volume é também o volume atual.");
+        } else {
+            volume = new Integer(novoVolume.byteValue());
+            System.out.println("Volume alterado para " + volume);
+        }
+    }
+
+    // Estamos aproveitando a classe Televisor para fazer nosso teste
+    public static void main(String[] args) {
+        Televisor tv = new Televisor();
+
+        // Não deveria mudar o volume e canal
+        tv.mudarVolume(20);
+        tv.mudarCanal(130);
+
+        // Deveria mudar o volume e canal
+        tv.mudarVolume(300);
+        tv.mudarCanal(10);
+
+        System.out.println("aparece ai fi");
+    }
+
 }
